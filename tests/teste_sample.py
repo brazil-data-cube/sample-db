572f10d55bcbf4d55a38d7cb0d57f52927105706
Fabiana Zioti <fabiana_zioti@hotmail.com>
2021-02-24 17:10:58 -0300

    preparing for test

---
+++ tests/teste_sample.py
@@ -0,0 +1,13 @@
+#
+# This file is part of Sample Database Model.
+# Copyright (C) 2020-2021 INPE.
+#
+# Sample Database Model is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Unit-test for sample model."""
+
+
+def test_query():
+    pass
