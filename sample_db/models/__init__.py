3b7affdaaca7d60b396281f58fc4514d68b64282
Fabiana Zioti <fabiana_zioti@hotmail.com>
2021-03-01 09:00:22 -0300

    fixing data in header

--- sample_db/models/__init__.py
+++ sample_db/models/__init__.py
@@ -1,6 +1,6 @@
 #
 # This file is part of Sample Database Model.
-# Copyright (C) 2020-21 INPE.
+# Copyright (C) 2020-2021 INPE.
 #
 # Sample Database Model is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
